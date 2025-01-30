78bc86bec19416b3c5534dbf6e1fb2aba8685b57
projectmai1 <mai1g4mez@gmail.com>
2025-01-30 03:45:42 +0000

    Initial commit

---
+++ src/components/DeleteConfirmation.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+
+interface DeleteConfirmationProps {
+  open?: boolean;
+  onConfirm?: () => void;
+  onCancel?: () => void;
+  title?: string;
+  description?: string;
+}
+
+const DeleteConfirmation = ({
+  open = true,
+  onConfirm = () => {},
+  onCancel = () => {},
+  title = "Konfirmasi Hapus Data",
+  description = "Apakah Anda yakin ingin menghapus data ini? Tindakan ini tidak dapat dibatalkan.",
+}: DeleteConfirmationProps) => {
+  return (
+    <AlertDialog open={open}>
+      <AlertDialogContent className="bg-white">
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel
+            onClick={onCancel}
+            className="bg-gray-100 hover:bg-gray-200"
+          >
+            Batal
+          </AlertDialogCancel>
+          <AlertDialogAction
+            onClick={onConfirm}
+            className="bg-red-600 hover:bg-red-700"
+          >
+            Hapus
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default DeleteConfirmation;
